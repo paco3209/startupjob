8342faec42efe722cbe05fb8228693db138902fa
fran <frano4240>
2020-05-29 01:20:57 -0300

    mongodb

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
-    mongoURI:process.env.MONGO_URI
-}
+    //mongoURI: "mongodb://127.0.0.1:27017/job"
+    
+    mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-porvz.mongodb.net/test?retryWrites=true&w=majority`
+  }
+  
