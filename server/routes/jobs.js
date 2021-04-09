345592cce562d1bfe2e3bf1b573fc46f022a05c1
fran <frano4240>
2021-04-09 03:09:29 -0300

    redeploy heroku

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -139,17 +139,17 @@ router.post("/jobsbyid", (req, res) => {
     let type = req.query.type
     let productIds = req.query.id
 
-    console.log(productIds)
+    
     if (type === "array") {
         let ids = req.query.id.split(',');
         productIds = [];
         productIds = ids.map(item => {
-            return item
+            return item  
         })
     }
     
     //we need to find the product information that belong to product Id 
-    Job.find({ '_id': { $in: productIds } })
+    Job.find({ '_id': { $in: productIds } }) 
     //.populate('writer')
     .exec((err, product) => {
         if(err) return req.status(400).send(err)
